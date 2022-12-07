bc81dabfee445653b4cb39416b20ec547ce56878
GabrijelaJuricic <gabjuricic@gmail.com>
2022-12-07 14:53:36 +0100

    Add basic layout to MyOrders component

--- src/components/MyOrders.js
+++ src/components/MyOrders.js
@@ -1,13 +1,44 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import alphaLogo from "../assets/alphaLogo.png";
+import motorcycle_2 from "../assets/motorcycle_2.png";
+import alpha from "../assets/alpha.png";
+import "./MyOrders.css";
 
 const MyOrders = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
-      MyOrders{" "}
-      <button onClick={() => navigate("/new-order")}>Go to NewOrder</button>
+    <div className="my-orders-container">
+      <div className="my-orders-left">
+        <div className="alpha-header">
+          <div className="alpha-logo">
+            <img src={alphaLogo} />
+            <h1>alpha</h1>
+          </div>
+          <div className="create-new-order-btn">
+            <button onClick={() => navigate("/new-order")}>
+              Create New Order
+            </button>
+          </div>
+        </div>
+        <div className="my-orders-title">
+          <h3>My Orders</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div className="my-orders-list">
+          <p>You have no orders.</p>
+          <Link to="/new-order">Create New Order</Link>
+        </div>
+      </div>
+      <div className="my-orders-right">
+        <img
+          alt="motorcycle_2"
+          className="my-orders-motorcycle_2"
+          src={motorcycle_2}
+        />
+        <img alt="alpha" className="my-orders-alpha" src={alpha} />
+      </div>
     </div>
   );
 };
