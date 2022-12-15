ceac7553b3a26c15086b0a9eb2f18d193c4d3936
GabrijelaJuricic <gabjuricic@gmail.com>
2022-12-15 16:34:57 +0100

    Display created orders in table
    It is a draft. For now, most of the data that is shown
    in the table is dummy data.

--- src/pages/MyOrders.js
+++ src/pages/MyOrders.js
@@ -3,15 +3,36 @@ import { Link, useNavigate } from "react-router-dom";
 import alphaLogo from "../assets/alphaLogo.png";
 import motorcycle_2 from "../assets/motorcycle_2.png";
 import alpha from "../assets/alpha.png";
+import TableCell from "@mui/material/TableCell";
+import {
+  Table,
+  TableBody,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import "./MyOrders.css";
+import { useRecoilValue } from "recoil";
+import { enteredMileageState, selectedModelState } from "../atoms";
 
 const MyOrders = () => {
+  const selectedModel = useRecoilValue(selectedModelState);
+  const mileage = useRecoilValue(enteredMileageState);
   const navigate = useNavigate();
 
   const createNewOrderHandler = () => {
     navigate("/new-order");
   };
 
+  const rows = [
+    {
+      orderId: "orderId",
+      serviceDate: "dd/mm/yy",
+      selectedModel: selectedModel,
+      mileage: mileage,
+      progress: "cancelled",
+    },
+  ];
   return (
     <div className="my-orders-container">
       <div className="my-orders-left">
@@ -28,10 +49,43 @@ const MyOrders = () => {
           <h3>My Orders</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
-        <div className="my-orders-list">
+        {/* <div className="my-orders-list">
           <p>You have no orders.</p>
           <Link to="/new-order">Create New Order</Link>
-        </div>
+        </div> */}
+        <TableContainer sx={{ width: 900 }}>
+          <Table aria-label="customized table">
+            <TableHead sx={{ paddingLeft: 0 }}>
+              <TableRow>
+                <TableCell>Order ID</TableCell>
+                <TableCell align="left">Service Date</TableCell>
+                <TableCell align="left">Model</TableCell>
+                <TableCell align="left">Mileage</TableCell>
+                <TableCell align="left">Progress</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow>
+                  <TableCell component="th" scope="row">
+                    {row.orderId}
+                  </TableCell>
+                  <TableCell align="left">{row.serviceDate}</TableCell>
+                  <TableCell align="left">{row.selectedModel}</TableCell>
+                  <TableCell align="left">{row.mileage}</TableCell>
+                  <TableCell align="left">{row.progress}</TableCell>
+                  <TableCell align="center">
+                    {
+                      <a href="#" style={{ textDecoration: "none" }}>
+                        Description
+                      </a>
+                    }
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
       <div className="my-orders-right">
         <img
