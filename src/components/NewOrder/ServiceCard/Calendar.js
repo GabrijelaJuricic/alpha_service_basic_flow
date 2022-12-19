d0dfc73f487340ac2fb18b771b2a160289041771
GabrijelaJuricic <gabjuricic@gmail.com>
2022-12-19 12:27:56 +0100

    Implement Date time picker from Material UI

--- src/components/NewOrder/ServiceCard/Calendar.js
+++ src/components/NewOrder/ServiceCard/Calendar.js
@@ -1,7 +1,27 @@
 import React from "react";
+import dayjs from "dayjs";
+import TextField from "@mui/material/TextField";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 
 const Calendar = () => {
-  return <div>Calendar</div>;
+  const [value, setValue] = React.useState(dayjs(new Date()));
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateTimePicker
+        label="New appointment"
+        value={value}
+        onChange={handleChange}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
+  );
 };
 
 export default Calendar;
