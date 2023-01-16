635856c523ee58a88ce629e2be2cc11368f9720d
GabrijelaJuricic <gabjuricic@gmail.com>
2023-01-16 11:24:17 +0100

    Add overflow to the Table

--- src/components/MyOrders/Table.js
+++ src/components/MyOrders/Table.js
@@ -9,28 +9,30 @@ const Table = () => {
   const collectedValues = useRecoilValue(collectValuesState);
 
   return (
-    <table>
-      <thead>
-        <tr>
-          {TABLE_HEADER.map((title, index) => {
-            return <th key={index}>{title}</th>;
+    <div className="table-container">
+      <table>
+        <thead>
+          <tr>
+            {TABLE_HEADER.map((title, index) => {
+              return <th key={index}>{title}</th>;
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {collectedValues.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>{item.orderId}</td>
+                <td>{item.serviceDay}</td>
+                <td>{item.brand}</td>
+                <td>{item.model}</td>
+                <td>{item.mileage}</td>
+              </tr>
+            );
           })}
-        </tr>
-      </thead>
-      <tbody>
-        {collectedValues.map((item, index) => {
-          return (
-            <tr key={index}>
-              <td>{item.orderId}</td>
-              <td>{item.serviceDay}</td>
-              <td>{item.brand}</td>
-              <td>{item.model}</td>
-              <td>{item.mileage}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
